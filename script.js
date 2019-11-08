4937bf19a92705f27c45a9b1fca7c6f9b0f7a992
Brian Kan <BrianKan00@hotmail.com>
2019-11-08 15:27:52 -0500

    Working with .getElementsByTagName and .innerText

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.querySelector(selectors).match;
+
+
+
+let textRetrieval1 = document.getElementsByTagName("article")
+let textRetrieval2 = document.getElementsByTagName("article")[0]
+let textRetrieval3 = document.getElementsByTagName("article")[0].children
+let textRetrieval4 = document.getElementsByTagName("article")[0].innerText
+
+console.log(textRetrieval1)
+console.log(textRetrieval2)
+console.log(textRetrieval3)
+console.log(textRetrieval4)
